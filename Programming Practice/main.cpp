18ce3d6d3c63a96370b0792f32b959dc718ed502
broadway_lamb <seergy_pro@me.com>
2016-03-11 15:33:33 +0300

    Initial Commit

---
+++ Programming Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Programming Practice
+//
+//  Created by Sergej Jaskiewicz on 11.03.16.
+//  Copyright © 2016 Sergej Jaskiewicz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
